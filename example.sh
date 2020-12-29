f941323b5954d4823f28d7fc6d1b41f19486a5fd
IKoulierakis <45217879+koulyiannis@users.noreply.github.com>
2020-12-29 14:26:30 +0200

    Add files via upload

--- example.sh
+++ example.sh
@@ -8,4 +8,6 @@ make
 ./lif_nonlocal_diag example/example.out < example/params
 
 #plot results
-
+python3 freq.py example/example.dat
+gnuplot -e "file='example/example'" space_time_plot.gnu
+rm freq.out
