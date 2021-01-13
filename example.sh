507e780a30924b27ce98b594f5d28acc5d9c0e47
Koulierakis Yiannis <45217879+ikoulierakis@users.noreply.github.com>
2021-01-13 21:27:01 +0200

    Add files via upload

--- example.sh
+++ example.sh
@@ -4,6 +4,6 @@
 ./lif_nonlocal_diag example/example.dat < example/params
 
 #plot results
-python3 freq.py example/example.dat
+python3 freq.py example/example.dat 500 #calculates the mean phase velocity starting from 500 t.u.
 gnuplot -e "file='example/example'" space_time_plot.gnu
 rm freq.out
